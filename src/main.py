922319fa4f11ebf5118109466ac7cd4b4fc90b95
statusquonjc46 <nicholas.coletta@gmail.com>
2024-11-26 13:32:57 -0500

    added solution files....

--- src/main.py
+++ src/main.py
@@ -1,6 +1,13 @@
 from textnode import *
+from htmlnode import *
 def main():
     t_node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
-
     print(t_node)
+
+    h_node = HTMLNode(tag="<b>", value="stuff to test here", children=["h1", "p"], props={"href": "https://boot.dev", "target": "_blank"})
+    print (h_node)
+
+    l_node = LeafNode(tag="a", value="click me!", props={"href": "https://boot.dev"})
+    print(l_node)
+
 main()
